c2ebcf801580cdd7043575ee92a3abefce3787da
Konstantin <konpichugin@yandex.ru>
2020-01-21 17:54:24 +0200

    rename brain-games

---
+++ even.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BrainGames\Even;
+
+function even()
+{
+    echo "drfgcjk";
+}
--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,8 @@ use function cli\prompt;
 
 function run()
 {
-    line('Welcome');
+    line('Welcome to Brain Games!');
+    line('Answer "yes" if the number is even, otherwise answer "no".');
     $name = prompt('May I have your name?');
     line('Hello, %s!', $name);
 }
