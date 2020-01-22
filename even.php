0154fdb4297ee888458d17f5f5652d1369f2a934
Konstantin <konpichugin@yandex.ru>
2020-01-22 21:48:10 +0200

    ready logic

--- even.php
+++ even.php
@@ -2,7 +2,38 @@
 
 namespace BrainGames\Even;
 
+use function cli\line;
+use function cli\prompt;
+
 function even()
 {
-    echo "drfgcjk";
+
+    line('Welcome to Brain Games!');
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    $name = prompt('May I have your name?');
+    line("Hello, %s", $name);
+
+
+   for ($i = 0; $i < 3; $i++) {
+       
+       $number = rand(1, 100);
+
+       if ($number % 2 === 0) {
+        $answerCorrect = "yes";
+       } else {
+        $answerCorrect = "no";
+       }		
+       	
+      line('Question:'.$number);
+      $answer = prompt('You answer');
+
+      if ($answer !== $answerCorrect) {
+         line("'{$answer}' is wrong answer ;). Correct answer was '{$answerCorrect}'");
+	      exit;		      
+      }
+      line("Correct!");
+   }
+      line("Congratulations, $name");	    
 }
+
+
