3b03c21d396b38b4332611a1369a31f9bd8c084c
Konstantin <konpichugin@yandex.ru>
2020-02-25 12:08:30 +0200

    add function in even.php

--- src/games/even.php
+++ src/games/even.php
@@ -6,36 +6,41 @@ namespace BrainGames\Even;
 
 use function cli\line;
 use function cli\prompt;
-/**
- * Функция определение четности
- */
-function even()
+
+function subEven($number)
 {
+    if ($number % 2 === 0) {
+                $answerCorrect = "yes";
+            } else {
+                $answerCorrect = "no";
+	    }
+    return $answerCorrect;
+}
+
+
+function even()
+{	
     line('Welcome to Brain Games!');
     line('Answer "yes" if the number is even, otherwise answer "no".');
     $name = prompt('May I have your name?');
     line("Hello, %s", $name);
 
-
     for ($i = 0; $i < 3; $i++) {
        
         $number = rand(1, 100);
+	$answerCorrect = subEven($number);
 
-        if ($number % 2 === 0) {
-            $answerCorrect = "yes";
-        } else {
-            $answerCorrect = "no";
-        }
         line('Question:'.$number);
         $answer = prompt('You answer');
 
         if ($answer !== $answerCorrect) {
             line("'{$answer}' is wrong answer ;).Correct answer was '{$answerCorrect}'");
             exit;		      
-        }
+	}
+
         line("Correct!");
     }
-        line("Congratulations, $name");	    
+
+    line("Congratulations, $name");
+        	    
 }
-
-
